2f6717b9723bbbfa718fe98c68a2cd26f2adbfc7
Gemnz29 <167627614+Gemnz29@users.noreply.github.com>
2025-09-03 01:29:42 +0700

    Rematch

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour {
 
@@ -7,11 +8,22 @@ public class GameOverUI : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI resultTextMesh;
     [SerializeField] private Color winColor;
     [SerializeField] private Color loseColor;
+    [SerializeField] private Button rematchButton;
 
 
+    private void Awake() {
+        rematchButton.onClick.AddListener(() => {
+            GameManager.Instance.RematchRpc();
+        });
+    }
     private void Start() {
         GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+        GameManager.Instance.OnRematch += GameManager_OnRematch;
 
+        Hide();
+    }
+
+    private void GameManager_OnRematch(object sender, System.EventArgs e) {
         Hide();
     }
 
--- Assets/Scripts/GameVisualManager.cs
+++ Assets/Scripts/GameVisualManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -12,9 +13,28 @@ public class GameVisualManager : NetworkBehaviour {
     [SerializeField] private Transform lineCompletePrefab;
 
 
+    private List<GameObject> visualGameObjectList;
+
+
+    private void Awake() {
+        visualGameObjectList = new List<GameObject>();
+    }
+
     private void Start() {
         GameManager.Instance.OnClickedOnGridPosition += GameManager_OnClickedOnGridPosition;
         GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+        GameManager.Instance.OnRematch += GameManager_OnRematch;
+    }
+
+    private void GameManager_OnRematch(object sender, System.EventArgs e) {
+        if (!NetworkManager.Singleton.IsServer) {
+            return;
+        }
+
+        foreach (GameObject visualGameObject in visualGameObjectList) {
+            Destroy(visualGameObject);
+        }
+        visualGameObjectList.Clear();
     }
 
     private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e) {
@@ -37,6 +57,8 @@ public class GameVisualManager : NetworkBehaviour {
                 Quaternion.Euler(0, 0, eulerZ)
             );
         lineCompleteTransform.GetComponent<NetworkObject>().Spawn(true);
+
+        visualGameObjectList.Add(lineCompleteTransform.gameObject);
     }
 
     private void GameManager_OnClickedOnGridPosition(object sender, GameManager.OnClickedOnGridPositionEventArgs e) {
@@ -59,6 +81,8 @@ public class GameVisualManager : NetworkBehaviour {
         }
         Transform spawnedCrossTransform = Instantiate(prefab, GetGridWorldPosition(x, y), Quaternion.identity);
         spawnedCrossTransform.GetComponent<NetworkObject>().Spawn(true);
+
+        visualGameObjectList.Add(spawnedCrossTransform.gameObject);
     }
 
     private Vector2 GetGridWorldPosition(int x, int y) {
